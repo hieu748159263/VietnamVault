e700bc3d2c7fc3cfa336e4ab6b7b603439a461b2
hieu748159263 <hieu748159263@gmail.com>
2023-01-08 19:04:11 +0700

    Modify JSON format

--- src/main/java/App.java
+++
@@ -1,5 +0,0 @@
-public class App {
-    public static void hello() {
-        System.out.println("Hello World!");
-    }
-}
--- src/main/java/data/util/AppTest.java
+++
@@ -1,5 +0,0 @@
-public class AppTest {
-    public static void main(String[] args) {
-        App.hello();
-    }
-}
