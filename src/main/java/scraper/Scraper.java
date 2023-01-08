d107fae70681069ce028901765a1b8076efd65d4
hieu748159263 <hieu748159263@gmail.com>
2023-01-08 18:44:47 +0700

    Modify JSON format

--- src/main/java/scraper/Scraper.java
+++ src/main/java/scraper/Scraper.java
@@ -1,26 +1,67 @@
-package scrape;
+package scraper;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 import com.gargoylesoftware.htmlunit.html.HtmlTableCell;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 
-@JsonPropertyOrder({ "link", "xpath", "regex", "output_attribute_name" })
-public class ScrapeObject {
+public class Scraper {
+
+    private String csvpath;
+    private String outputPath;
+
+    public Scraper(String csvpath, String outputPath) {
+        this.csvpath = csvpath;
+        this.outputPath = outputPath;
+    }
+
+    public void getJsonFromCSV() throws Exception {
+        File csvFile = new File(csvpath);
+
+        CsvMapper csvMapper = new CsvMapper();
+        CsvSchema csvSchema = csvMapper
+                .typedSchemaFor(ScrapeObject.class)
+                .withHeader()
+                .withColumnSeparator(',');
+
+        MappingIterator<ScrapeObject> scrapeObjectIter = csvMapper
+                .readerWithTypedSchemaFor(ScrapeObject.class)
+                .with(csvSchema)
+                .readValues(csvFile);
+        List<ScrapeObject> scrapeObjects = scrapeObjectIter.readAll();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        for (ScrapeObject scrapeObject : scrapeObjects) {
+            try (PrintWriter output = new PrintWriter(outputPath + "\\" + scrapeObject.getType() + ".json")) {
+                String jsonString = gson.toJson(JsonParser.parseString(scrapeObject.scrape().toString()));
+                output.println(jsonString);
+            }
+        }
+    }
+}
+
+@JsonPropertyOrder({"link","xpath","regex","type"})
+class ScrapeObject {
 
     private String link;
     private String xpath;
     private String regex;
-    private String outputAttributeName;
-
-    public ScrapeObject() {
-    }
+    private String type;
 
     public String getLink() {
         return link;
@@ -34,8 +75,8 @@ public class ScrapeObject {
         return regex;
     }
 
-    public String getOutputAttributeName() {
-        return outputAttributeName;
+    public String getType() {
+        return type;
     }
 
     public JSONArray scrape() throws Exception {
@@ -53,7 +94,7 @@ public class ScrapeObject {
                 List<HtmlTableCell> header = table.getRow(0).getCells();
                 List<String> attrName = header.stream()
                         .map(cell -> cell.asNormalizedText().replaceAll(regex, ""))
-                        .toList();
+                        .collect(Collectors.toList());
 
                 for (int i = 1; i < table.getRows().size(); i++) {
                     JSONObject jsonObject = new JSONObject();
--- src/test/java/ScraperTest.java
+++ src/test/java/ScraperTest.java
@@ -1,8 +1,8 @@
-import scrape.Scraper;
+import scraper.Scraper;
 
 public class ScraperTest {
     public static void main(String[] args) throws Exception {
-        Scraper scraper = new Scraper("src/test/resources/scrapeObject.csv");
+        Scraper scraper = new Scraper("src\\main\\resources\\csv\\scrapeObject.csv", "src\\main\\resources\\data");
         scraper.getJsonFromCSV();
     }
 }
