2330e6da6170949aafbee7692c60c81a70d3c222
Duong <daokimduong322@gmail.com>
2023-01-15 18:51:00 +0700

    add dynasty, festival, site object with SerializedName

---
+++ src/main/java/historyObjects/Festival.java
@@ -0,0 +1,53 @@
+package historyObjects;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Festival {
+
+	@SerializedName("Lễ hội truyền thống")
+	private String festivalName;
+	
+	@SerializedName("Ngày âm lịch")
+	private String lunarCalendarDate;
+	
+	@SerializedName("Ghi chú")
+	private String note;
+	
+	@SerializedName("Lần đầu tổ chức năm")
+	private String theFirstTime_Year;
+	
+	@SerializedName("Vị trí")
+	private String location;
+
+	public String getFestivalName() {
+		return festivalName;
+	}
+
+	public String getLunarCalendarDate() {
+		return lunarCalendarDate;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public String getTheFirstTime_Year() {
+		return theFirstTime_Year;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	@Override
+	public String toString() {
+		return "Festival{"
+				+ "festivalName='" + festivalName + "\'"
+				+ ", lunarCalendarDate='" + lunarCalendarDate + "\'"
+				+ ", note='" + note + "\'"
+				+ ", theFirstTime_Year='" + theFirstTime_Year + "\'"
+				+ ", location=" + location + "\'"
+			+ "}";
+	}
+	
+}
---
+++ src/main/java/historyObjects/Site.java
@@ -0,0 +1,53 @@
+package historyObjects;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Site {
+
+	@SerializedName("Loại di tích")
+	private String typeOfSite;
+	
+	@SerializedName("Di tích")
+	private String name;
+	
+	@SerializedName("Ghi chú")
+	private String note;
+	
+	@SerializedName("Vị trí")
+	private String location;
+	
+	@SerializedName("Năm CN")
+	private String recognizedYear;
+
+	public String getTypeOfSite() {
+		return typeOfSite;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public String getRecognizedYear() {
+		return recognizedYear;
+	}
+
+	@Override
+	public String toString() {
+		return "Site{"
+				+ "typeOfSite='" + typeOfSite + "\'"
+				+ ", name=" + name + "\'"
+				+ ", note=" + note + "\'"
+				+ ", location=" + location+ "\'"
+				+ ", recognizedYear=" + recognizedYear + "\'"
+			+ "}";
+	}
+	
+}
